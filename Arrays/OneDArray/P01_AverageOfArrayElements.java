5038f5c84d1696884f97a1e111add7289a15cc64
MeghaChauhan278 <cmegha482@gmail.com>
2026-06-06 00:43:56 +0530

    Average of array elements

---
+++ Arrays/OneDArray/P01_AverageOfArrayElements.java
@@ -0,0 +1,14 @@
+/*Question:Find the average of array elements. */
+package Arrays.OneDArray;
+
+public class P01_AverageOfArrayElements {
+    public static void main(String[] args) {
+        int[] a={10,15,5};
+        int sum=0;
+        for(int i=0;i<a.length;i++){
+            sum+=a[i];
+        }
+        int avg=sum/a.length;
+        System.out.println(avg);
+    }
+}
