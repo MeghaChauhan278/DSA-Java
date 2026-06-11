664931a8e26dba061789cefe9191247e6c442e4a
MeghaChauhan278 <cmegha482@gmail.com>
2026-06-12 01:49:05 +0530

    maximum  sum of a subarray of size k

--- Arrays/TwoPointer/P10_SortColor.java
+++ Arrays/TwoPointer/P10_SortColor.java
@@ -5,6 +5,7 @@ Example 1:
 Input: nums = [2,0,2,1,1,0]
 Output: [0,0,1,1,2,2]                     in actual ques we dont have to return anything*/
 
+package Arrays.TwoPointer;
 import java.util.Arrays;
 
 public class P10_SortColor {
