6e478eaddc9147407b2cf045339d1e20d216599c
MeghaChauhan278 <cmegha482@gmail.com>
2026-06-23 23:29:42 +0530

    Find the Duplicate Number

---
+++ LinkedList/SlowAndFast/P04_FindDuplicateNo.java
@@ -0,0 +1,25 @@
+/*287. Find the Duplicate Number
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+There is only one repeated number in nums, return this repeated number.
+You must solve the problem without modifying the array nums and using only constant extra space.
+Example 1:
+Input: nums = [1,3,4,2,2] ,Output: 2 */
+package LinkedList.SlowAndFast;
+
+public class P04_FindDuplicateNo {
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[slow]];
+            if (fast == slow) {
+                slow = 0;
+                while (slow != fast) {
+                    slow = nums[slow];
+                    fast = nums[fast];
+                }
+                return slow;
+            }
+        }
+    }
+}
