39d8707049b1123590c0e620c3bb0f66c3192e75
MeghaChauhan278 <cmegha482@gmail.com>
2026-07-09 19:56:19 +0530

    Reverse a String

---
+++ Stack/P01_ReverseAString.java
@@ -0,0 +1,26 @@
+/*Reverse a String
+You are given a string s, and your task is to reverse the string.
+Examples: Input: s = "Geeks"
+Output: "skeeG"
+Constraints:
+1 <= s.size() <= 106
+s contains only alphabetic characters (both uppercase and lowercase). */
+package Stack;
+import java.util.*;
+public class P01_ReverseAString {
+    public static void main(String[] args) {
+        String s = "Geeks";
+        Stack<Character> st=new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            st.push(s.charAt(i));
+        }
+        String ans="";
+        while (!st.isEmpty()) {
+            ans+=st.pop();
+        }
+        System.out.println(ans);
+    }
+}
+
+//T.C=O(n)
