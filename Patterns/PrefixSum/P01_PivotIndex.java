920b5749d7ea5bcbe984be04c896b72fadebf38b
MeghaChauhan278 <cmegha482@gmail.com>
2026-08-08 16:16:41 +0530

    Find Pivot Index

--- Patterns/PrefixSum/P01_PivotIndex.java
+++ Patterns/PrefixSum/P01_PivotIndex.java
@@ -5,28 +5,34 @@ index is equal to the sum of all the numbers strictly to the index's right.
 If the index is on the left edge of the array, then the left sum is 0 because there are 
 no elements to the left. This also applies to the right edge of the array.
 Return the leftmost pivot index. If no such index exists, return -1.
-Example 1: Input: nums = [1,7,3,6,5,6]
-Output: 3
-Explanation:
-The pivot index is 3.
+Example 1: Input: nums = [1,7,3,6,5,6] , Output: 3
+Explanation: The pivot index is 3.
 Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
-Right sum = nums[4] + nums[5] = 5 + 6 = 11 */
+Right sum = nums[4] + nums[5] = 5 + 6 = 11 , Constraints: 1 <= nums.length <= 104
+-1000 <= nums[i] <= 1000*/
 package Patterns.PrefixSum;
-
+import java.util.*;
 public class P01_PivotIndex{
-    public static void main(String[] args) {
-        int[] nums={1,7,3,6,5,6};
-        int left=0,sum=0;
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
+    public static int pivotIndex(int[] nums) {
+        int left=0;
+        int sum=0;
+        for(int num:nums){
+            sum+=num;
         }
-
         for(int i=0;i<nums.length;i++){
             int right=sum-nums[i]-left;
             if(left==right){
-                System.out.println(i);
+                return i;
             }
             left+=nums[i];
         }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums={1,7,3,6,5,6};
+        int ans=pivotIndex(nums);
+        System.out.println(ans);
     }
 }
+// Time Complexity = O(n)
+// Space Complexity = O(1)
