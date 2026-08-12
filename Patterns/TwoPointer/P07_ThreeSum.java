590939b10227343663f2be97699d1b2c6ba0f8ee
MeghaChauhan278 <cmegha482@gmail.com>
2026-08-12 12:20:53 +0530

    3Sum

--- Patterns/TwoPointer/P07_ThreeSum.java
+++ Patterns/TwoPointer/P07_ThreeSum.java
@@ -9,43 +9,53 @@ nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
 nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
 nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
 The distinct triplets are [-1,0,1] and [-1,-1,2].
-Notice that the order of the output and the order of the triplets does not matter. */
+Notice that the order of the output and the order of the triplets does not matter.
+Constraints: 3 <= nums.length <= 3000   ,   -105 <= nums[i] <= 105 */
 package Patterns.TwoPointer;
+
 import java.util.*;
+
 public class P07_ThreeSum {
-    public static void main(String[] args) {
-        int[] a={-1,0,1,2,-1,-4};
-        Arrays.sort(a);
-        List<List<Integer>> result=new ArrayList<>();
-        for(int i=0;i<a.length-2;i++){
-            if(i>0 && a[i]==a[i-1]){
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);//O(n log n)
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int start=i+1;
-            int end=a.length-1;
-            int target=-a[i];
 
-            while(start<end){
-                int sum=a[start]+a[end];
-                if(sum==target){
-                    result.add(Arrays.asList(a[i],a[start],a[end]));
-                    start++;
-                    end--;
+            int low = i + 1, high = nums.length - 1, sum = 0;
+            int target = -nums[i];
+            while (low < high) {
+                sum = nums[low] + nums[high];
+                if (sum == target) {
+                    res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    low++;
+                    high--;
 
-                    while(start<a.length && a[start]==a[start-1]){
-                        start++;
+                    while (low < high && nums[low] == nums[low - 1]) {
+                        low++;
                     }
-                    while (start<end && a[end] == a[end+1]){
-                        end--;
+                    while (low < high && nums[high] == nums[high + 1]) {
+                        high--;
                     }
-                }
-                else if(sum<target){
-                    start++;
-                }else{
-                    end--;
+                } else if (sum < target) {
+                    low++;
+                } else {
+                    high--;
                 }
             }
         }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -1, 0, 1, 2, -1, -4 };
+        List<List<Integer>> result = threeSum(nums);
         System.out.println(result);
     }
 }
+// T.c=O(n^2)
+// S.C = O(1) auxiliary space
+// Output space = O(n²) in the worst case
