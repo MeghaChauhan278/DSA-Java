19f4f5979e2525e67aff367ff3c0d573470da1d5
MeghaChauhan278 <cmegha482@gmail.com>
2026-08-15 11:42:47 +0530

    Minimum Number of Arrows to Burst Balloons

---
+++ Patterns/MergeInterval/P07_MinNoOfArrowsToBurstBalloons.java
@@ -0,0 +1,55 @@
+/*452. Minimum Number of Arrows to Burst Balloons
+There are some spherical balloons taped onto a flat wall that represents the XY-plane.
+The balloons are represented as a 2D integer array points where points[i] = [xstart, xend] 
+denotes a balloon whose horizontal diameter stretches between xstart and xend. You do not 
+know the exact y-coordinates of the balloons.
+Arrows can be shot up directly vertically (in the positive y-direction) from different points
+along the x-axis. A balloon with xstart and xend is burst by an arrow shot at x if xstart <= x <= xend.
+There is no limit to the number of arrows that can be shot. A shot arrow keeps traveling up infinitely,
+bursting any balloons in its path.
+Given the array points, return the minimum number of arrows that must be shot to burst all balloons.
+Example 1:
+Input: points = [[10,16],[2,8],[1,6],[7,12]]
+Output: 2
+Explanation: The balloons can be burst by 2 arrows:
+- Shoot an arrow at x = 6, bursting the balloons [2,8] and [1,6].
+- Shoot an arrow at x = 11, bursting the balloons [10,16] and [7,12].
+Example 2:
+Input: points = [[1,2],[3,4],[5,6],[7,8]]
+Output: 4
+Explanation: One arrow needs to be shot for each balloon for a total of 4 arrows.
+Constraints:
+1 <= points.length <= 105
+points[i].length == 2
+-231 <= xstart < xend <= 231 - 1 */
+package Patterns.MergeInterval;
+import java.util.*;
+public class P07_MinNoOfArrowsToBurstBalloons {
+    public static int findMinArrowShots(int[][] points){
+        Arrays.sort(points,(a,b)->Integer.compare(a[0],b[0]));
+        int st1=points[0][0];
+        int end1=points[0][1];
+        int count=1;
+
+        for(int i=1;i<points.length;i++){
+            int st2=points[i][0];
+            int end2=points[i][1];
+
+            if(end1>=st2){
+                end1=Math.min(end1, end2);
+            }else{
+                count++;
+                end1=end2;
+                st1=st2;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[][] points = {{10,16},{2,8},{1,6},{7,12}};
+        int ans=findMinArrowShots(points);
+        System.out.println(ans);
+    }
+}
+// TC: O(n log n)
+// SC: O(log n) 
