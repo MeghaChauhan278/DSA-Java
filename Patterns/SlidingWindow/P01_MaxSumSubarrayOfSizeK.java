97057cddf0bd19980814524203d644f2f8be1922
MeghaChauhan278 <cmegha482@gmail.com>
2026-08-16 18:56:41 +0530

    Max Sum Subarray of size K

--- Patterns/SlidingWindow/P01_MaxSumSubarrayOfSizeK.java
+++ Patterns/SlidingWindow/P01_MaxSumSubarrayOfSizeK.java
@@ -1,31 +1,42 @@
-/*Question:Given an array of integers arr[]  and a number k. Return the maximum
- sum of a subarray of size k.
-Note: A subarray is a contiguous part of any given array.
-Examples:
-Input: arr[] = [100, 200, 300, 400], k = 2
+/*Max Sum Subarray of size K
+Given an array of integers arr[]  and a number k. Return the maximum sum of a subarray of size k.
+Examples: Input: arr[] = [100, 200, 300, 400], k = 2
 Output: 700
-Explanation: arr2 + arr3 = 700, which is maximum. */
+Explanation: arr2 + arr3 = 700, which is maximum.
+Input: arr[] = [1, 4, 2, 10, 23, 3, 1, 0, 20], k = 4
+Output: 39
+Explanation: arr1 + arr2 + arr3 + arr4 = 39, which is maximum.
+Constraints:
+1 ≤ arr.size() ≤ 106
+0 ≤ arr[i] ≤ 106
+1 ≤ k ≤ arr.size()*/
 package Patterns.SlidingWindow;
-
 public class P01_MaxSumSubarrayOfSizeK {
-    public static void main(String[] args) {
-        int[] a={100, 200, 300, 400};
-        int k=2;
-
-        int sum=0;
-        int low=0,high=k-1;
-        for(int i=0;i<=high;i++){//this loop calculates the sum of the first window of size k.
-            sum+=a[i];
+    public static int maxSubarraySum(int[] arr, int k){
+        int high=k-1,low=0,res=0,sum=0;
+        for(int i=0;i<k;i++){
+            sum+=arr[i];
         }
-        int result=sum;
-        while(high<a.length-1){//sliding
+        res=sum;
+        while(high<arr.length-1){
             low++;
-            sum-=a[low-1];
+            sum-=arr[low-1];
             high++;
-
-            sum+=a[high];
-            result=Math.max(sum, result);
+            sum+=arr[high];
+            res=Math.max(res, sum);
         }
-        System.out.println(result);
+        return res;
+    }
+    public static void main(String[] args) {
+        int[] arr1={100, 200, 300, 400};
+        int k1=2;
+        int[] arr2 = {1, 4, 2, 10, 23, 3, 1, 0, 20};
+        int k2=4;
+        int ans1=maxSubarraySum(arr1,k1);
+        System.out.println(ans1);
+        int ans2=maxSubarraySum(arr2,k2);
+        System.out.println(ans2);
     }
 }
+//T.C=O(n)
+//S.C=O(1)
