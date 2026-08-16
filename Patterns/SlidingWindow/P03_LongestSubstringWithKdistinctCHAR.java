3939b5658768bcd344b7935b9c214d5f1881a895
MeghaChauhan278 <cmegha482@gmail.com>
2026-08-16 19:38:08 +0530

    Longest Substring with K Uniques

--- Patterns/SlidingWindow/P03_LongestSubstringWithKdistinctCHAR.java
+++ Patterns/SlidingWindow/P03_LongestSubstringWithKdistinctCHAR.java
@@ -1,43 +1,52 @@
-/*Question: You are given a string s consisting only lowercase alphabets and an integer k. 
-Your task is to find the length of the longest substring that contains exactly k distinct characters.
+/*Longest Substring with K Uniques
+You are given a string s consisting only lowercase alphabets and an integer k. Your task is to find the 
+length of the longest substring that contains exactly k distinct characters.
 Note : If no such substring exists, return -1. 
-Examples:
-Input: s = "aabacbebebe", k = 3
+Examples: Input: s = "aabacbebebe", k = 3
 Output: 7
-Explanation: The longest substring with exactly 3 distinct characters is "cbebebe", which includes 'c', 'b', and 'e'. */
+Explanation: The longest substring with exactly 3 distinct characters is "cbebebe", which includes 'c', 'b', and 'e'.
+Input: s = "aaaa", k = 2
+Output: -1
+Explanation: There's no substring with 2 distinct characters.
+Constraints:
+1 ≤ s.size() ≤ 105
+1 ≤ k ≤ 26 */
 package Patterns.SlidingWindow;
 import java.util.HashMap;
 //Sliding.W +hashmap
 public class P03_LongestSubstringWithKdistinctCHAR {
-    public static void main(String[] args) {
-        String s = "aabacbebebe";
-        int k=3;
-
+    public static int longestKSubstr(String s, int k){
         HashMap<Character,Integer> map=new HashMap<>();
-
         int low=0,high=0,res=-1;
-
         while(high<s.length()){
             char ch=s.charAt(high);
-            map.put(ch, map.getOrDefault(ch,0)+1);
-
-            while (map.size()>k) {
-                char leftchar=s.charAt(low);
-                map.put(leftchar, map.get(leftchar)-1);
-
-                if(map.get(leftchar)==0){
-                    map.remove(leftchar);
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+            while(map.size()>k){
+                char ls=s.charAt(low);
+                map.put(ls,map.get(ls)-1);
+                if(map.get(ls)==0){
+                    map.remove(ls);
                 }
                 low++;
-                
             }
             if (map.size()==k) {
-                int len=high-low+1;
-                res=Math.max(res, len);
-                
+                res=Math.max(res, high-low+1);
             }
             high++;
         }
-        System.out.println(res);
+        return res;
+    }
+    public static void main(String[] args) {
+        String s1 = "aabacbebebe";
+        int k1=3;
+        String s2 = "aaaa";
+        int k2=2;
+        int ans1=longestKSubstr(s1,k1);
+        int ans2=longestKSubstr(s2,k2);
+        System.out.println(ans1);
+        System.out.println(ans2);
+
     }
 }
+//T.C=O(n)
+//S.C=O(n)
